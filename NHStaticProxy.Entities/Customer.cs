1b2d24cb9cd2b7f8ca89e590979d2820008a51c9
Zoubi <michael.delva@gmail.com>
2011-06-17 10:57:24 +0200

    Added NHStaticProxy.Entities project

---
+++ NHStaticProxy.Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using Iesi.Collections.Generic;
+
+namespace NHStaticProxy.Entities
+{
+    [StaticProxy]
+    public class Customer
+    {
+        public Customer()
+        {
+            Orders = new HashedSet<Order>();
+        }
+        
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public ISet<Order> Orders { get; set; }
+        
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+            order.Customer = this;
+        }
+    }
+}
